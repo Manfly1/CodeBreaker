78b8a3b13dc0a0fc63ca25c75a2681b0831adae3
Manfly1 <aivanuyk0@gmail.com>
2021-10-05 09:29:25 +0300

    add difficulties

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -7,5 +7,23 @@ module Codebreaker
     attr_reader :clues, :user, :difficulty,
                 :attempts_used, :hints_used, :very_secret_code,
                 :date
-  end
-end
+
+    DIFFICULTIES = {
+      easy: { attempts: 15, hints: 2 },
+      medium: { attempts: 10, hints: 1 },
+      hell: { attempts: 5, hints: 1 }
+    }.freeze
+
+    CODE_LENGTH = 4
+    RANGE_GUESS_CODE = (1..6).freeze
+
+    def initialize(difficulty:, user:, date: Date.today)
+      validate_difficulty(difficulty, DIFFICULTIES)
+
+      @user = user
+      @difficulty = difficulty
+      @date = date
+
+      attempts
+      number_of_hints
+    end
