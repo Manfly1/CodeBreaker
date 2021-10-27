80679b78b7f5ca9df5867febc4b9c5f3253da32f
Manfly1 <aivanuyk0@gmail.com>
2021-10-27 14:15:13 +0300

    refactoring storage

--- lib/codebreaker/storage.rb
+++ lib/codebreaker/storage.rb
@@ -7,53 +7,45 @@ module Codebreaker
     FILE_DIRECTORY = 'statistics'
     FILE_NAME = 'statistics.yml'
 
-    def save_file(game)
-      
-      create_directory
-      rating = load_file
-      rating << game_data(game)
-      store = YAML::Store.new(storage_path)
-      store.transaction do
-        store[:codebrakers] = rating
-      end
-      store
+    attr_accessor :data
+
+    def initialize
+      @data = db_initialized? ? load : initialize_db
     end
 
-    def load_file
-      YAML.load_file(storage_path)[:codebrakers] || []
-    rescue Errno::ENOENT
-      []
+    def save
+      store = YAML::Store.new(storage_path)
+      store.transaction { @data.each { |key, value| store[key] = value } }
+    end
+
+    private
+
+    def db_initialized?
+      Dir.exist?(FILE_DIRECTORY) && File.file?(File.join(FILE_DIRECTORY, FILE_NAME))
+    end
+
+    def initialize_db
+      Dir.mkdir(FILE_DIRECTORY)
+
+      store = YAML::Store.new(File.join(FILE_DIRECTORY, FILE_NAME))
+      store.transaction { default_data.each { |key, value| store[key] = value } }
+
+      default_data
+    end
+
+    def default_data
+      {
+        user_statistics: []
+      }
     end
 
     def storage_path
       File.join(FILE_DIRECTORY, FILE_NAME)
     end
 
-    private
-
-    def fetch_user_data(game)
-      user_data = {}
-      user_data[:name] = game.user.name
-      user_data[:difficulty] = game.difficulty
-      user_data[:attempts] = game.difficulty.attempts,
-      user_data[:attempts_used] = game.difficulty.attempts - @game.attempts,
-      user_data[:hints] = @game.difficulty.hints
-      user_data[:hints_used] = @game.difficulty.hints - @game.hints
-      user_data
-    end
-
-    def create_directory
-      Dir.mkdir(FILE_DIRECTORY) unless Dir.exist?(FILE_DIRECTORY)
-      File.open(FILE_NAME, 'w') unless File.exist?(FILE_NAME)
-    end
-
-    def game_data(game)
-      game_results = {}
-      game_results[:name] = game.user.name
-      game_results[:difficulty] = game.difficulty
-      game_results[:available_attempts] = game.difficulty.attempts
-      game_results[:available_hints] = game.difficulty.hints
-      game_results.merge(fetch_user_data(game))
+    def load
+      store = YAML::Store.new(storage_path)
+      store.transaction { store.roots.to_h { |key| [key, store[key]] } }
     end
   end
 end
